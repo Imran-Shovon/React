a2a11f910737ec6b12e9be200b3f19c778747525
shovon-gt <shovon.biswas@gigatechltd.com>
2025-06-08 17:03:37 +0600

    Episode 3

--- app.js
+++ app.js
@@ -2,18 +2,39 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 //React Element
-//React.createElement => React Element -> Object => HTMLElement(render)
-const heading = React.createElement("h1", { id: "heading"}, "This is a React Element");
-console.log(heading);
+const Title = () => (<h1 id ="heading">Component using JSX.</h1>)
+
+//React Element
+const lm = <span>React Element outside</span>
+const title = <h1 id ="heading">
+    {lm}. React Element.
+</h1>;
 
 
-//JSX(transpiled before it reaches the JS engine) by Parcel(Babel do this) - is not HTML in JavaScript --XML like syntax
-//JSX is a syntax extension for JavaScript that looks similar to HTML
-//JSX => Babel transpiled it to React.createElement => React Element -> JS Object => HTMLElement(render)
-const headingJSX = (<h1 id="heading" className="head">
-    This is a JSX Element
-    </h1>);
-console.log(headingJSX);
+//Using normal JavaScript function
+// const Title = function(){
+//     return (
+//         <h1 id ="heading">Component using JSX.</h1>
+//     )
+// }
+
+//React Component
+//Class based component - OLD way
+//Functional component - NEW way
+
+
+//Functional component
+//Component composition
+const HeadingComponent = () => (
+    <div id= "container">
+        {title}
+        { Title() }
+        <Title />
+        <Title></Title>
+        <h1 id = "heading">Component composition and functional component.</h1>
+    </div>
+)
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(headingJSX);
+root.render(<HeadingComponent />);
